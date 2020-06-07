153d30554579eec2f335862f1fc5aa66188ab9ca
dbaluch <dominika.baluch@gmail.com>
2020-06-08 00:04:50 +0200

    anonimizacja oceny zajęć, skrypt do backupu

--- infra/db_backups/anonymize.sql
+++ infra/db_backups/anonymize.sql
@@ -1,6 +1,3 @@
--- Nuke IBANs
-UPDATE users_studiazamawiane SET bank_account='';
-
 -- Anonymize first/last names
 UPDATE auth_user SET first_name=CONCAT(SUBSTRING(first_name, 1, 1), '_', id);
 UPDATE auth_user SET last_name=CONCAT(SUBSTRING(last_name, 1, 1), '_', id);
@@ -15,7 +12,5 @@ DELETE FROM mailer_messagelog;
 
 -- Make sure everyone has the same simple password, in this case, 'pass'
 -- See here: https://docs.djangoproject.com/en/2.0/topics/auth/passwords/
-UPDATE auth_user SET password='pbkdf2_sha256$36000$Z6GlerjZ9cWC$M6zn6XGPc81913R1yw6SMouredUfO/DPnQwZ3XxUCnA=';
-
--- Anonymize grade/poll answers
-DELETE FROM poll_submission;
+UPDATE auth_user SET 
+password='pbkdf2_sha256$36000$Z6GlerjZ9cWC$M6zn6XGPc81913R1yw6SMouredUfO/DPnQwZ3XxUCnA=';
--- infra/db_backups/slack_notifications.py
+++ infra/db_backups/slack_notifications.py
@@ -1,29 +1,22 @@
-#!/usr/bin/env python3
 from slack import WebClient
 
 
-def get_connected_slack_client(secrets_env):
-    slack_client = WebClient(token=secrets_env.str('SLACK_TOKEN'))
-    return slack_client
-
-    # for real time connection
-    # if slack_client.rtm_connect(with_team_state=False):
-    #     return slack_client
-    #raise RuntimeError('SlackClient.rtm_connect failed')
+def connect_slack_client(slack_token):
+    slack_client = WebClient(slack_token)
+    if slack_client.rtm_connect(with_team_state=False):
+        return slack_client
+    raise RuntimeError('SlackClient.rtm_connect failed')
 
 
-def _send_slack_msg(slack_client, channel_id: str, msg: str):
-    slack_client.chat_postMessage(
-        channel=channel_id,
-        text=msg
-    )
+def send_slack_msg(slack_client, chan, msg: str):
+    slack_client.api_call('chat.postMessage', channel=chan, text=msg)
 
 
-def send_success_notification(slack_client, dev_db_link: str, seconds_elapsed: int, channel_id: str):
+def send_success_notification(slack_client, dev_db_link: str, seconds_elapsed: int):
     msg = f'Databases backed up successfully in {seconds_elapsed} seconds. *Dev DB download link:* {dev_db_link}'
-    _send_slack_msg(slack_client, channel_id, msg)
+    send_slack_msg(slack_client, 'db_backups', msg)
 
 
-def send_error_notification(slack_client, error_msg: str, channel_id: str):
+def send_error_notification(slack_client, error_msg: str):
     msg = f'*Failed to back up databases:*\n```{error_msg}```'
-    _send_slack_msg(slack_client, channel_id, msg)
+    send_slack_msg(slack_client, 'bugs', msg)
