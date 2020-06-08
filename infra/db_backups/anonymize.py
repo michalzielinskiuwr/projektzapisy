5a94908e4cfa20f59bcd96d6d7ef0a33cfcc0fa2
dbaluch <dominika.baluch@gmail.com>
2020-06-08 12:11:08 +0200

    Poprawki długości linii

--- infra/db_backups/anonymize.py
+++ infra/db_backups/anonymize.py
@@ -1,8 +1,11 @@
 import json
 
+<<<<<<< HEAD:infra/db_backups/anonymize.py
 import psycopg2
 from loremipsum import generate_paragraph
 
+=======
+>>>>>>> Poprawki długości linii:db_backups/anonymize.py
 
 def anonymize_poll(conn):
     """Performs anonymization of poll submissions.
@@ -31,15 +34,40 @@ def anonymize_poll(conn):
     polls = cur.fetchall()
     for row in polls:
         cur.execute("update poll_submission set answers = %s where id = %s",
+<<<<<<< HEAD:infra/db_backups/anonymize.py
                     (jsons_dict[row[1]], row[0]))
+=======
+            (jsons_dict[row[1]], row[0]))
+>>>>>>> Poprawki długości linii:db_backups/anonymize.py
     cur.close()
     conn.commit()
 
 
+<<<<<<< HEAD:infra/db_backups/anonymize.py
 def connect_and_anonymize(user, password, db_name, port):
+=======
+def connect_and_anonymize():
+    if len(sys.argv) < 5:
+        sys.exit(1)
+>>>>>>> Poprawki długości linii:db_backups/anonymize.py
     try:
         conn = psycopg2.connect(dbname=db_name, user=user,
                                 password=password, host="localhost", port=port)
         anonymize_poll(conn)
+<<<<<<< HEAD:infra/db_backups/anonymize.py
     except psycopg2.Error:
         raise
+=======
+    except Exception as e:
+        print(str(e))
+        sys.exit(1)
+
+
+def main():
+    conn = connect_and_anonymize()
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
+>>>>>>> Poprawki długości linii:db_backups/anonymize.py
