a9b23f3a145844ba6f6b39e3e1733234218b7ed3
Jan Marcinkowski <jasiekmarc@gmail.com>
2020-10-29 14:09:36 +0100

    Usuwa odchodzący argument `providing_args` do `Signal()`

--- zapisy/apps/notifications/custom_signals.py
+++ zapisy/apps/notifications/custom_signals.py
@@ -1,8 +1,18 @@
 import django.dispatch
 
-student_pulled = django.dispatch.Signal(providing_args=["instance", "user"])
-student_not_pulled = django.dispatch.Signal(
-    providing_args=["instance", "user", "reason"])
-teacher_changed = django.dispatch.Signal(
-    providing_args=["instance", "teacher"])
-thesis_voting_activated = django.dispatch.Signal(providing_args=["instance"])
+# Signal senders must provide `instance` and `user` arguments. `instance` must be
+#   instance: apps.enrollment.courses.models.Group
+#   user: django.contrib.auth.models.User
+student_pulled = django.dispatch.Signal()
+# Signal senders must provide the following arguments:
+#   instance: apps.enrollment.courses.models.Group
+#   user: django.contrib.auth.models.User
+#   reason: str
+student_not_pulled = django.dispatch.Signal()
+# Signal senders must provide the following arguments:
+#   instance: apps.enrollment.courses.models.Group
+#   teacher: apps.users.models.Employee
+teacher_changed = django.dispatch.Signal()
+# Signal senders must provide an argument
+#   instance: apps.theses.models.Thesis
+thesis_voting_activated = django.dispatch.Signal()
