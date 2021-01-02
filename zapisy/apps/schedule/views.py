f2702f61655bce92df21d167d8c988711de3e7c9
Konrad <299243@uwr.edu.pl>
2021-01-02 21:32:40 +0100

    Add deleting events without authentication. Code cleanup

--- zapisy/apps/schedule/views.py
+++ zapisy/apps/schedule/views.py
@@ -45,18 +45,14 @@ def exams(request):
     pass
 
 
-# TODO: Ask if GET parameters checking is necessary. 404 vs 500 response
-# TODO: normal user cannot see other people unaccepted events, he sees unaccepted self created events
-#  Admin can see others unaccepted events
-# Sends only required data for fullcalendar
-def terms(request):
-    # GET parameters checking
+def _check_and_prepare_get_data(request):
+    data = {}
     try:
-        start = datetime.strptime(request.GET.get('start'), '%Y-%m-%dT%H:%M:%S.%fZ')
-        end = datetime.strptime(request.GET.get('end'), '%Y-%m-%dT%H:%M:%S.%fZ')
-        visible = bool(request.GET.get('visible', True))
-        place = str(request.GET.get('place', ''))
-        title_or_author = str(request.GET.get('title_author', ''))
+        data['start'] = datetime.strptime(request.GET.get('start'), '%Y-%m-%dT%H:%M:%S.%fZ')
+        data['end'] = datetime.strptime(request.GET.get('end'), '%Y-%m-%dT%H:%M:%S.%fZ')
+        data['visible'] = bool(request.GET.get('visible', True))
+        data['place'] = str(request.GET.get('place', ''))
+        data['title_or_author'] = str(request.GET.get('title_author', ''))
         types = request.GET.get('types', [Event.TYPE_GENERIC])
         types = types.split(',') if isinstance(types, str) else types
         if not isinstance(types, list):
@@ -64,6 +60,7 @@ def terms(request):
         for type_ in types:
             if not any(type_ == t for t, _ in Event.TYPES):
                 raise ValueError
+        data['types'] = types
         statuses = request.GET.get('statuses', [Event.STATUS_ACCEPTED])
         statuses = statuses.split(',') if isinstance(statuses, str) else statuses
         if not isinstance(statuses, list):
@@ -71,6 +68,7 @@ def terms(request):
         for status in statuses:
             if not any(status == s for s, _ in Event.STATUSES):
                 raise ValueError
+        data['statuses'] = statuses
         rooms = request.GET.get('rooms', [])
         rooms = rooms.split(',') if rooms else rooms
         if not isinstance(rooms, list):
@@ -78,31 +76,38 @@ def terms(request):
         for room in rooms:
             if not isinstance(room, str):
                 raise ValueError
+        data['rooms'] = rooms
+        return data
     except (ValueError, TypeError):
         raise Http404
 
-    query = Term.objects.filter(day__range=[start, end]).select_related('event')
-    rooms = Classroom.objects.filter(number__in=rooms) if rooms else None
+
+# TODO: Ask if GET parameters checking is necessary. 404 vs 500 response
+# TODO: normal user cannot see other people unaccepted events, he sees unaccepted self created events
+#  Admin can see others unaccepted events
+# Sends only required data for fullcalendar
+def terms(request):
+    data = _check_and_prepare_get_data(request)
+    query = Term.objects.filter(day__range=[data['start'], data['end']]).select_related('event')
+    rooms = Classroom.objects.filter(number__in=data['rooms']) if data['rooms'] else None
     if rooms:
         query = query.filter(room__in=rooms)
-    if place:
-        query = query.filter(place=place)
-    if types:
-        query = query.filter(event__type__in=types)
-    if visible:
-        query = query.filter(event__visible=visible)
-    if statuses:
-        query = query.filter(event__status__in=statuses)
-    if title_or_author:
-        author_names = title_or_author.split()
+    if data['place']:
+        query = query.filter(place=data['place'])
+    if data['types']:
+        query = query.filter(event__type__in=data['types'])
+    if data['statuses']:
+        query = query.filter(event__status__in=data['statuses'])
+    if data['title_or_author']:
+        author_names = data['title_or_author'].split()
         first_name = author_names[0]
         last_name = author_names[-1]
-        query = query.filter(Q(event__title__icontains=title_or_author) |
+        query = query.filter(Q(event__title__icontains=data['title_or_author']) |
                              Q(event__author__first_name__icontains=first_name) |
                              Q(event__author__first_name__icontains=last_name) |
                              Q(event__author__last_name__icontains=first_name) |
                              Q(event__author__last_name__icontains=last_name))
-
+    query = query.filter(event__visible=data['visible'])
     payload = []
     for term in query:
         event = term.event
@@ -116,18 +121,20 @@ def terms(request):
     return JsonResponse(payload, safe=False)
 
 
-# gets json, same structure like in GET
-# TODO: transaction atomic for creating Event and Terms
-# TODO: Is creating normal class events required? Ask
-@csrf_exempt
-def events_post(request):
+def _check_and_prepare_post_payload(request):
+    """Check if payload has proper keys nad values for creating and updating events"""
     payload = json.loads(request.body)
+    checked_payload = {}
+    # TODO: change author to request.user
+    checked_payload['author'] = User.objects.get(first_name='M_74', last_name='B_74')
+    # TODO: change Event status to STATUS_PENDING, STATUS_ACCEPTED should be only if author is admin?
+    checked_payload['status'] = Event.STATUS_ACCEPTED
     try:
-        title = str(payload.get('title', ''))
-        description = str(payload.get('description', ''))
-        visible = bool(payload.get('visible', True))
-        type_ = payload.get('type', [Event.TYPE_GENERIC])
-        if not any(type_ == t for t, _ in Event.TYPES):
+        checked_payload['title'] = str(payload.get('title', ''))
+        checked_payload['description'] = str(payload.get('description', ''))
+        checked_payload['visible'] = bool(payload.get('visible', True))
+        checked_payload['type'] = payload.get('type', [Event.TYPE_GENERIC])
+        if not any(checked_payload['type'] == t for t, _ in Event.TYPES):
             raise ValueError
         terms = payload.get('terms', [])
         if not isinstance(terms, list):
@@ -143,14 +150,22 @@ def events_post(request):
                 raise ValueError
             if not any(k in term for k in ('room', 'place')):
                 raise KeyError
+        checked_payload['terms'] = terms
+        return checked_payload
     except (ValueError, TypeError, KeyError):
         raise Http404
 
-    # TODO: change author to request.user and change Event status to STATUS_PENDING
-    author = User.objects.get(first_name='M_74', last_name='B_74')
-    event = Event.objects.create(title=title, author=author, description=description, type=type_, visible=visible,
-                                 status=Event.STATUS_ACCEPTED)
-    for term in terms:
+
+# gets json, same structure like in GET
+# TODO return url to new event?
+# TODO: transaction atomic for creating Event and Terms
+# TODO: Is creating normal class events required? Ask
+@csrf_exempt
+def create_event(request):
+    payload = _check_and_prepare_post_payload(request)
+    event = Event.objects.create(title=payload['title'], author=payload['author'], description=payload['description'],
+                                 type=payload['type'], visible=payload['visible'], status=payload['status'])
+    for term in payload['terms']:
         room = Classroom.objects.get(number=term['room']) if 'room' in term else None
         place = term['place'] if 'place' in term else None
         term = Term(event=event, day=term["start"].date(), start=term["start"].time(), end=term["end"].time())
@@ -160,7 +175,7 @@ def events_post(request):
             term.place = place
         term.save()
     # return HttpResponseBadRequest()
-    return HttpResponse(status=201)
+    return HttpResponse("<html><body>Event created</body></html>", status=201)
 
 
 # TODO: time filtering or semester filtering. Add sorting by created or edited date if necessary
@@ -170,46 +185,24 @@ def events_post(request):
 @csrf_exempt
 def events(request):
     if request.method == "POST":
-        return events_post(request)
-    # GET parameters checking
-    try:
-        visible = bool(request.GET.get('visible', True))
-        title_or_author = str(request.GET.get('title_author', ''))
-        types = request.GET.get('types', [Event.TYPE_GENERIC])
-        types = types.split(',') if isinstance(types, str) else types
-        if not isinstance(types, list):
-            raise TypeError
-        for type_ in types:
-            if not any(type_ == t for t, _ in Event.TYPES):
-                raise ValueError
-        statuses = request.GET.get('statuses', [Event.STATUS_ACCEPTED])
-        statuses = statuses.split(',') if isinstance(statuses, str) else statuses
-        if not isinstance(statuses, list):
-            raise TypeError
-        for status in statuses:
-            if not any(status == s for s, _ in Event.STATUSES):
-                raise ValueError
-    except (ValueError, TypeError):
-        raise Http404
-
+        return create_event(request)
+    data = _check_and_prepare_get_data(request)
     # TODO: events filtering, similar to Event.get_event_or_404(event_id, request.user) but for many events,
     query = Event.objects.filter().select_related('author')
-    if types:
-        query = query.filter(type__in=types)
-    if visible:
-        query = query.filter(visible=visible)
-    if statuses:
-        query = query.filter(status__in=statuses)
-    if title_or_author:
-        author_names = title_or_author.split()
+    if data['types']:
+        query = query.filter(type__in=data['types'])
+    if data['statuses']:
+        query = query.filter(status__in=data['statuses'])
+    if data['title_or_author']:
+        author_names = data['title_or_author'].split()
         first_name = author_names[0]
         last_name = author_names[-1]
-        query = query.filter(Q(title__icontains=title_or_author) |
+        query = query.filter(Q(title__icontains=data['title_or_author']) |
                              Q(author__first_name__icontains=first_name) |
                              Q(author__first_name__icontains=last_name) |
                              Q(author__last_name__icontains=first_name) |
                              Q(author__last_name__icontains=last_name))
-
+    query = query.filter(visible=data['visible'])
     payload = []
     for event in query:
         terms = event.term_set.all().select_related('room')
@@ -236,7 +229,26 @@ def events(request):
     return JsonResponse(payload, safe=False)
 
 
+@csrf_exempt
+def delete_event(request, event_id):
+    Event.objects.get(pk=event_id).delete()
+    return HttpResponse("<html><body>Event deleted</body></html>", status=200)
+
+
+@csrf_exempt
+def update_event(request, event_id):
+    # TODO
+    return HttpResponse("<html><body>Event updated</body></html>", status=200)
+
+
+# TODO: normal user cannot see other people unaccepted events, he sees unaccepted self created events
+#  Admin can see others unaccepted events
+@csrf_exempt
 def event(request, event_id):
+    if request.method == "POST":
+        return update_event(request, event_id)
+    if request.method == "DELETE":
+        return delete_event(request, event_id)
     event = Event.get_event_or_404(event_id, request.user)
     terms = event.term_set.all().select_related('room')
     author = event.author
