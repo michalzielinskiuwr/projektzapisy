2f46bfbefff1d5e13a82916cd52d388d107956b8
Tomasz Woszczyński <duchowe50k@gmail.com>
2021-01-20 16:29:20 +0100

    Add filtering by event type and coloring events based on their type, status

--- zapisy/apps/schedule/assets/fullcalendar.js
+++ zapisy/apps/schedule/assets/fullcalendar.js
@@ -5,11 +5,40 @@ import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 
+const event_status = {
+    "pending":  "0",
+    "accepted": "1",
+    "rejected": "2"
+};
+
+const event_type = {
+    "exam":  "0",
+    "test":  "1",
+    "event": "2",
+    "class": "3",
+    "other": "4",
+    "special_reservation": "5"
+};
+
+const event_color = {
+    "invisible": "#d3bb5a",
+    "user_is_author": "#8ed35a",
+    "pending": "#f00", // "#c371ef",
+    "accepted": "#0f0",
+    "rejected": "#c13333",
+    // Type colors are used to mark events
+    "type_exam":  "#ff0",
+    "type_test":  "#ff3399",
+    "type_event": "#0066ff",
+    "type_class": "#003300",
+    "type_other": "#ffcc00",
+    "type_special_reservation": "#009900"
+};
+
 async function fetchEvents(fetchInfo) {
-    // let url = new URL("http://127.0.0.1:8000/classrooms/terms/");
-    let url = new URL("http://192.168.0.16:8000/classrooms/terms/");
-    url.searchParams.set("start", fetchInfo.start.toISOString())
-    url.searchParams.set("end", fetchInfo.end.toISOString())
+    let url = new URL("classrooms/terms/", window.location.origin);
+    url.searchParams.set("start", fetchInfo.start.toISOString());
+    url.searchParams.set("end", fetchInfo.end.toISOString());
 
     let room_id = $('#room_selector').val();
     if (room_id === "all")
@@ -21,13 +50,49 @@ async function fetchEvents(fetchInfo) {
     if (title_author_input)
         url.searchParams.set("title_author", title_author_input);
 
-    const response = await fetch(url);
-    return response.json();
+    var types = [];
+    if ($('#exam_checkbox').is(':checked'))                 types.push(event_type.exam);
+    if ($('#test_checkbox').is(':checked'))                 types.push(event_type.test);
+    if ($('#event_checkbox').is(':checked'))                types.push(event_type.event);
+    if ($('#classes_checkbox').is(':checked'))              types.push(event_type.class);
+    if ($('#other_checkbox').is(':checked'))                types.push(event_type.other);
+    if ($('#special_reservation_checkbox').is(':checked'))  types.push(event_type.special_reservation);
+
+    if (types.length > 0)
+        url.searchParams.set("types", types);
+    else
+        return [];
+
+    var fetchedEvents = $.getJSON(url, function (data) {
+        for (var i = 0; i < data.length; i++) {
+            // Set colors for fetched events
+            switch (data[i]['type']) {
+                case event_type.exam:  data[i]['color'] = event_color.type_exam;
+                                       data[i]['textColor'] = "black"; break;
+                case event_type.test:  data[i]['color'] = event_color.type_test; break;
+                case event_type.event: data[i]['color'] = event_color.type_event; break;
+                case event_type.class: data[i]['color'] = event_color.type_class; break;
+                case event_type.other: data[i]['color'] = event_color.type_other; break;
+                case event_type.special_reservation: data[i]['color'] = event_color.type_special_reservation; break;
+            }
+
+            if(data[i]['visible'] == false)
+                data[i]['color'] = event_color.invisible;
+
+            switch (data[i]['status']) {
+                case event_status.pending: data[i]['borderColor'] = event_color.pending; break;
+                case event_status.accepted: data[i]['borderColor'] = event_color.accepted; break;
+                case event_status.rejected: data[i]['borderColor'] = event_color.rejected; break;
+            }
+        }
+    });
+    return fetchedEvents;
 }
 
 // Function handler for creating events
 function create_event(timeRange) {
     $('#create_event_modal').modal('show');
+    calendar.unselect();
     document.getElementById("reservation_date").valueAsDate = new Date();
 
     // timeRange is specified for events created by selecting hours in calendar
@@ -37,6 +102,14 @@ function create_event(timeRange) {
         $('#reservation_date').val(start.slice(0, 10));
         $('#reservation_time_start').val(start.slice(11, 16));
         $('#reservation_time_end').val(end.slice(11, 16));
+
+        calendar.addEvent({
+            id: "temp",
+            title: "Nowe wydarzenie (wstępny termin)",
+            start: start,
+            end: end,
+            color: "pink"
+        })
     }
 
     $('#create_event_button').off('click');
@@ -44,24 +117,22 @@ function create_event(timeRange) {
         var name = $('#reservation_name').val().toString();
         var description = $('#reservation_description').val().toString();
         var visibility = ($('#reservation_visibility').val() == 'visible') ? true : false;
-        var room = ($('#reservation_room').val() != 'room_none') ? $('#reservation_room').val() : null;
+        var rooms = ($('#reservation_room').val() != 'room_none') ? $('#reservation_room').val() : null;
         var place = ($('#reservation_room').val() == 'room_none') ? $('#reservation_place').val() : null;
         var type;
-        var startTime = new Date($('#reservation_date').val() + "T" + $('#reservation_time_start').val() + "Z").toISOString();
-        var endTime = new Date($('#reservation_date').val() + "T" + $('#reservation_time_end').val() + "Z").toISOString();
         var day = $('#reservation_date').val();
         var start = $('#reservation_time_start').val();
         var end = $('#reservation_time_end').val();
-        
+
         switch ($('#reservation_type').val()) {
             case "type_event":
-                type = "2";
+                type = event_type.event;
                 break;
             case "type_exam":
-                type = "0";
+                type = event_type.exam;
                 break;
             case "type_test":
-                type = "1";
+                type = event_type.test;
                 break;
         }
 
@@ -79,26 +150,15 @@ function create_event(timeRange) {
             ]
         };
 
-        // We accept only room or only place, both cannot be specified at the same time
-        if (room)
-            eventData.terms[0]["room"] = parseInt(room);
-        else
-            eventData.terms[0]["place"] = place;
+        eventData.terms[0]["room"] = rooms;
+        eventData.terms[0]["place"] = place ? place : "";
 
-        // FullCalendar needs its own object to add the event to calendar, these fields
-        // have to be named title, start and end.
-        var fullCalendarData = {
-            title: name,
-            start: startTime,
-            end: endTime,
-            color: "green"
-        }
-        calendar.addEvent(fullCalendarData);
-
-        $.post("http://192.168.0.16:8000/classrooms/events/", JSON.stringify(eventData), null, "json");
+        console.log("???", JSON.stringify(eventData));
+        $.post("events/", JSON.stringify(eventData), calendar.refetchEvents(), "json");
 
         $('#create_event_modal').modal('hide');
         calendar.unselect();
+        calendar.refetchEvents();
     });
 }
 
@@ -137,7 +197,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
         selectable: true,
         selectMirror: true,
-        select: create_event
+        select: create_event,
+
+        eventClick: function (info) {
+            // info.jsEvent.preventDefault(); // do not redirect to given url, display info in modal
+            console.log(info.event.title);
+        }
     });
 
     calendar.render();
@@ -154,6 +219,10 @@ document.addEventListener("DOMContentLoaded", function() {
         create_event(null);
     });
 
+    $('#event_type').contents().find(':checkbox').bind('change', function () {
+        calendar.refetchEvents();
+    })
+
     // Show input for place only if no room is selected (create event modal)
     $(function () {
         $('#reservation_room').on('change', function () {
@@ -162,5 +231,10 @@ document.addEventListener("DOMContentLoaded", function() {
             else
                 $('#reservation_place_div').hide();
         });
+    });
+
+    $('#create_event_modal').on('hidden.bs.modal', function () {
+        if (calendar.getEventById("temp"))
+            calendar.getEventById("temp").remove();
     });
 });
