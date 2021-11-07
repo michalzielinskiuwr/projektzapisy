c7eaf61c61f7e7d1e6d547230b4298c38dfdc5dc
Aleksandra Rozkrut <arozkrut8@gmail.com>
2021-11-07 20:35:56 +0100

    add ImageManager app and models

---
+++ zapisy/apps/image_manager/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+
