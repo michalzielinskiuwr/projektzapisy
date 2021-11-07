5136e488a8bf9c7de3fb98c8b81ae8e0244007e2
Przemysław Hoszowski <przemyslaw.hoszowski@gmail.com>
2021-11-07 14:16:39 +0100

    wip: adding form which add/modify defect

--- zapisy/apps/defect/models.py
+++ zapisy/apps/defect/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+DEFECT_MAX_NAME_SIZE = 255
+DEFECT_MAX_PLACE_SIZE = 255
 
 
 class StateChoices(models.IntegerChoices):
@@ -10,9 +14,10 @@ class StateChoices(models.IntegerChoices):
 
 
 class Defect(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Tytuł')
-    date = models.DateTimeField(auto_now_add=True)
-    place = models.CharField(max_length=255, verbose_name="Miejsce")
+    name = models.CharField(max_length=DEFECT_MAX_NAME_SIZE, verbose_name='Nazwa')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_modification = models.DateTimeField(auto_now_add=True)
+    place = models.CharField(max_length=DEFECT_MAX_PLACE_SIZE, verbose_name="Miejsce")
     description = models.TextField("Opis usterki", blank=True)
     reporter = models.ForeignKey(User, on_delete=models.CASCADE)
     state = models.PositiveSmallIntegerField("Stan", choices=StateChoices.choices, default=StateChoices.CREATED)
