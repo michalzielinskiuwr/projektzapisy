ace6cc1814d25f03c2a8f14d66882fd00bb8b2ab
Disaster26 <lukstasiak26@wp.pl>
2022-02-22 20:11:48 +0100

    check2

--- zapisy/apps/enrollment/records/models/records.py
+++ zapisy/apps/enrollment/records/models/records.py
@@ -234,8 +234,8 @@ class Record(models.Model):
         waiting = queued.difference(enrolled)
         wt = []
         for query in waiting:
-               for query_all in to_combine:
-                      if ((query['group__course'] == query_all['group__course']) and (query['group__course'] == query_all['group__course']) and (query['group__type'] == query_all['group__type']) and (query['student__user'] == query_all['student__user']) and (query['student__user__first_name'] == query_all['student__user__first_name']) and (query['student__user__last_name'] == query_all['student__user__last_name'])): wt.append(query_all)
+                for query_all in to_combine:
+                       if ((query['group__course'] == query_all['group__course']) and (query['group__course'] == query_all['group__course']) and (query['group__type'] == query_all['group__type']) and (query['student__user'] == query_all['student__user']) and (query['student__user__first_name'] == query_all['student__user__first_name']) and (query['student__user__last_name'] == query_all['student__user__last_name'])): wt.append(query_all)
                       break
         ret = defaultdict(lambda: defaultdict(int))
         for w in wt:
