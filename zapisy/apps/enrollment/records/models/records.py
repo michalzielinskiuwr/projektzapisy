84d6141af8052359b89084126d152b0cf6ac46d5
Disaster26 <lukstasiak26@wp.pl>
2022-02-22 21:46:30 +0100

    check2

--- zapisy/apps/enrollment/records/models/records.py
+++ zapisy/apps/enrollment/records/models/records.py
@@ -236,7 +236,7 @@ class Record(models.Model):
         for query in waiting:
                 for query_all in to_combine:
                        if ((query['group__course'] == query_all['group__course']) and (query['group__course'] == query_all['group__course']) and (query['group__type'] == query_all['group__type']) and (query['student__user'] == query_all['student__user']) and (query['student__user__first_name'] == query_all['student__user__first_name']) and (query['student__user__last_name'] == query_all['student__user__last_name'])): wt.append(query_all)
-                      	       break
+                      	   break
         ret = defaultdict(lambda: defaultdict(int))
         for w in wt:
             ret[w['group__id']][w['group__type']] += 1
